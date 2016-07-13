d09e04053f765127b5c0ef41334bb29c2e6ec897
liante0904 <liante0904@gmail.com>
2016-07-13 10:17:26 +0900

    프로젝트 생성 commit

---
+++ AndEx00_Hello/app/src/main/java/com/exam/andex/andex00_hello/Relative_example01.java
@@ -0,0 +1,15 @@
+package com.exam.andex.andex00_hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016-07-13.
+ */
+public class Relative_example01 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_example01);
+    }
+}
