c628b0d47b7efe64f42ae9f13855b23fc635b4b0
liante0904 <liante0904@gmail.com>
2016-07-13 17:29:49 +0900

    새 프로젝트  생성 absolutelayout,  res를 이용한 이미지, text 색상 변경,  클릭시(이벤트)

---
+++ widgetMenu/app/src/main/java/com/exam/andex/widgetmenu/Select_ex01.java
@@ -0,0 +1,15 @@
+package com.exam.andex.widgetmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016-07-13.
+ */
+public class Select_ex01 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_ex01);
+    }
+}
---
+++ widgetMenu/app/src/main/java/com/exam/andex/widgetmenu/WidgetActivity.java
@@ -0,0 +1,13 @@
+package com.exam.andex.widgetmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WidgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button_ex01);
+    }
+}
