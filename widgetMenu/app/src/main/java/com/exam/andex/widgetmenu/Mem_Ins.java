c9091935454e18622d44928e41682a52a4ef3657
liante0904 <liante0904@gmail.com>
2016-07-22 18:24:27 +0900

    170722 마무리 commit

---
+++ widgetMenu/app/src/main/java/com/exam/andex/widgetmenu/Mem_Ins.java
@@ -0,0 +1,15 @@
+package com.exam.andex.widgetmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016-07-22.
+ */
+public class Mem_Ins extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mem_ins);
+    }
+}
--- widgetMenu/app/src/main/java/com/exam/andex/widgetmenu/Memlog.java
+++ widgetMenu/app/src/main/java/com/exam/andex/widgetmenu/Memlog.java
@@ -1,15 +1,23 @@
 package com.exam.andex.widgetmenu;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 /**
  * Created by Administrator on 2016-07-13.
  */
 public class Memlog extends Activity {
+
+    SQLiteDatabase db;
+    SQLiteOpenHelper helper;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mem_log);
     }
+
 }
